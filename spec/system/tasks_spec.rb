bc25aa9c44f21243a82b4ce32ecf0826933f206a
aoyama <syouta.aoyama20@gmail.com>
2020-11-01 01:08:46 +0900

    [Add] ログイン前にタスク編集ページへアクセスすると失敗するテストを作成。

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
 
+  let(:user) { create(:user) }
+  let(:task) { create(:task) }
+
   describe 'ログイン前' do
     describe 'タスク新規作成' do
       context 'ログインしていない状態' do
@@ -16,7 +19,9 @@ RSpec.describe "Tasks", type: :system do
     describe 'タスク編集' do
       context 'ログインしていない状態' do
         it '編集ページへのアクセスが失敗する' do
-
+          visit edit_task_path(task)
+          expect(current_path).to eq login_path
+          expect(page).to have_content('Login required')
         end
       end
     end
