15b8cde6b45d76d96ea709f2599e40d0f6b67371
aoyama <syouta.aoyama20@gmail.com>
2020-10-31 22:07:13 +0900

    [Add] ログインのテストを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -56,10 +56,19 @@ RSpec.describe "UserSessions", type: :system do
 
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
-      it 'ログイン処理が成功する'
+      it 'ログイン処理が成功する' do
+        sign_in_as user
+        expect(current_path).to eq root_path
+        expect(page).to have_content('Login successful')
+      end
     end
     context 'フォームが未入力' do
-      it 'ログイン処理が失敗する'
+      it 'ログイン処理が失敗する' do
+        visit login_path
+        click_button 'Login'
+        expect(current_path).to eq login_path
+        expect(page).to have_content('Login failed')
+      end
     end
   end
 
